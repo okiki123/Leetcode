747605a556ccb99f6f89320252404514cde34b53
okiki123 <okikimalomo17@gmail.com>
2022-09-06 17:02:13 +0100

    Updated Commit

---
+++ productOfArrayExceptItself.js
@@ -0,0 +1,23 @@
+/* Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation. */
+
+var productExceptSelf = function(nums) {
+    let res = [];
+
+    let product = 1;
+    for (let i = 0; i < nums.length; i++) {
+        res[i] = product;
+        product *= nums[i]
+    }
+
+    product = 1;
+    for(let j = nums.length -1; j >=0; j--) {
+        res[j] *= product;
+        product *= nums[j];
+    }
+
+    return res;
+}
