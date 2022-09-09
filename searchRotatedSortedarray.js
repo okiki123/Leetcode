6fe992f90b4f62d94efc85cef7ec92e9c846d597
okiki123 <okikimalomo17@gmail.com>
2022-09-09 10:42:41 +0100

    Updated Commit

--- searchRotatedSortedarray.js
+++ searchRotatedSortedarray.js
@@ -10,6 +10,19 @@ var search = function(nums, target) {
      let [ left, right ] = [ 0, nums.length - 1];
 
      while( left <= right ) {
-        
+        const mid = (left + right) / 2;
+        const guess = nums[mid];
+        const [ leftNum, rightNum ] = [ nums[left], nums[right] ];
+
+        const isTarget = guess === target;
+        if(isTarget) return mid;
+
+        const isAscending = leftNum <= guess;
+        if(isAscending) {
+            const isInRange = leftNum <= target;
+            const isLess = target < guess;
+
+            const isTargetGreater = !(isInRange && isLess);
+        }
      }
 }
