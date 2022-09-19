b7adcbe35ed8cfa49cd38244aafbfcff5f03fa6b
okiki123 <okikimalomo17@gmail.com>
2022-09-19 11:19:31 +0100

    Updated Commit

---
+++ sumOfTwoIntegers.js
@@ -0,0 +1,14 @@
+/* Given two integers a and b, return the sum of the two integers without using the operators + and -. */
+
+var getSum = function(a, b) {
+     let tb = b;
+     let res = a;
+
+     while(tb) {
+        let temp = (res & b) << 1;
+        res = res ^ tb;
+        tb = temp;
+     }
+
+     return res;
+}
